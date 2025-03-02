f4a85e166e8977412647b88d7c5b9c59130d2227
v4Miguel <124155033+v4Miguel@users.noreply.github.com>
2025-03-02 19:05:08 +0100

    Update __init__.py
    últimos cambios

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -1,4 +1,3 @@
-from .functions import pi_star_from_Q, pi_star_from_Q_recordVideo, plot, plot_episode_lengths
+from .functions import pi_star_from_Q, pi_star_from_Q_recordVideo, plot, plot_episode_lengths, embed_video, get_latest_episode_video_file
 
-
-__all__ = ['pi_star_from_Q', 'pi_star_from_Q_recordVideo', 'plot', 'plot_episode_lengths']
+__all__ = ['pi_star_from_Q', 'pi_star_from_Q_recordVideo', 'plot', 'plot_episode_lengths', 'embed_video', 'get_latest_episode_video_file']
