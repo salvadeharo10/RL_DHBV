805585733cff7a775f37d0dd4bf229e426bf938f
v4Miguel <124155033+v4Miguel@users.noreply.github.com>
2025-03-02 20:13:27 +0100

    Update __init__.py

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -1,3 +1,3 @@
-from .functions import pi_star_from_Q, pi_star_from_Q_recordVideo, plot, plot_episode_lengths, embed_video, get_latest_episode_video_file
+from .functions import pi_star_from_Q, pi_star_from_Q_recordVideo, plot, plot_episode_lengths, embed_video, get_latest_episode_video_file, ‎display_gif, frames_to_gif
 
-__all__ = ['pi_star_from_Q', 'pi_star_from_Q_recordVideo', 'plot', 'plot_episode_lengths', 'embed_video', 'get_latest_episode_video_file']
+__all__ = ['pi_star_from_Q', 'pi_star_from_Q_recordVideo', 'plot', 'plot_episode_lengths', 'embed_video', 'get_latest_episode_video_file','‎display_gif', 'frames_to_gif']
