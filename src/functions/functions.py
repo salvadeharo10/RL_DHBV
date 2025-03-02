847630fbdde2858e1271d481a9653e7809a61444
v4Miguel <124155033+v4Miguel@users.noreply.github.com>
2025-03-02 20:49:11 +0100

    Update functions.py
    cambios hechos

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -4,8 +4,6 @@ import os
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy.ndimage import uniform_filter1d
-from pyvirtualdisplay import Display
-from gymnasium.wrappers import RecordVideo
 import io
 import base64
 from IPython import display
@@ -16,35 +14,6 @@ import imageio
 
 # Definición de acciones
 LEFT, DOWN, RIGHT, UP = 0, 1, 2, 3
-
-# Función para mostrar videos en el notebook
-# Esta función mostrará un vídeos en la pantalla virtual, que será embembida en el notbook
-def embed_video(video_file):
-    # open and read the raw data from the video
-    video_data = io.open(video_file, 'r+b').read()
-    # now we have to encode the data into base64 to work
-    # with the virtual display
-    encoded_data = base64.b64encode(video_data)
-    # now we use the display.display function to take some html
-    # and the encoded data and embed the html into the notebook!
-    display.display(HTML(data=''''''.format(encoded_data.decode('ascii'))))
-    
-# Función para obtener el último video generado
-def get_latest_episode_video_file(directory):
-    import re
-    pattern = re.compile(r"rl-video-episode-(\d+)\.mp4")
-    latest_file = None
-    highest_episode = -1
-
-    for filename in os.listdir(directory):
-        match = pattern.match(filename)
-        if match:
-            episode_number = int(match.group(1))
-            if episode_number > highest_episode:
-                highest_episode = episode_number
-                latest_file = os.path.join(directory, filename)
-
-    return latest_file
 
 # Política Greedy a partir de los valores Q. Se usa para mostrar la solución.
 def pi_star_from_Q(env, Q):
@@ -63,7 +32,6 @@ def pi_star_from_Q(env, Q):
 
 def pi_star_from_Q_recordVideo(env, Q, video_folder="videos", num_episodes=5000):
     
-    env = RecordVideo(env, video_folder=video_folder, episode_trigger=lambda eid: True)
     print(f"Grabando ejecución...")
     done = False
     pi_star = np.zeros([env.observation_space.n, env.action_space.n])
