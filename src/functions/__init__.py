08f4013a223e1b7f4efd226a98c31fd577f10762
v4Miguel <124155033+v4Miguel@users.noreply.github.com>
2025-03-04 17:02:16 +0100

    Update __init__.py

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -1,3 +1,3 @@
-from .functions import pi_star_from_Q, pi_star_from_Q_recordVideo, plot, plot_episode_lengths, display_gif, frames_to_gif, plot_states_actions_distribution, plot_q_values_map
+from .functions import pi_star_from_Q, pi_star_from_Q_recordVideo, plot, plot_episode_lengths, display_gif, frames_to_gif, plot_states_actions_distribution, plot_q_values_map, pi_star_from_weights
 
-__all__ = ['pi_star_from_Q', 'pi_star_from_Q_recordVideo', 'plot', 'plot_episode_lengths', '‎display_gif', 'frames_to_gif', 'plot_states_actions_distribution', 'plot_q_values_map']
+__all__ = ['pi_star_from_Q', 'pi_star_from_Q_recordVideo', 'plot', 'plot_episode_lengths', '‎display_gif', 'frames_to_gif', 'plot_states_actions_distribution', 'plot_q_values_map', 'pi_star_from_weights']
